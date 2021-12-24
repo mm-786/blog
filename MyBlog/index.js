06634fb24a7a0e6530aa7a2ea703cf150357158c
M Mundrawala <mundrawala18@gmail.com>
2021-12-24 19:13:42 +0530

    blog

---
+++ MyBlog/index.js
@@ -0,0 +1,53 @@
+
+const express = require('express')
+const cors = require('cors')
+const { Base, Drive } = require('deta');
+const Post = Base('post')
+const app = express()
+app.use(express.json());
+app.use(cors())
+
+app.get('/', (req, res) => res.redirect("https://blog.mundrawala.ml"))
+
+app.post('/post', async (req, res) => {
+    var token = req.get('token')
+    if (token === 'mUrtAzA11mUnIrA22') {
+        const { title, content } = req.body;
+        const date = new Date().getDate() + "-" + (parseInt(new Date().getMonth())+1).toString() + "-" + new Date().getFullYear();
+        const toCreate = { title, date, content }
+        const key = title.replace(' ', '-').toLowerCase();;
+        const insertedPost = await Post.put(toCreate, key)
+        res.status(201).json(insertedPost);
+    }
+    else {
+        res.status(401).json({ "error": "Unauthorized" });
+    }
+})
+
+app.get('/posts', async (req, res) => {
+    var token = req.get('token')
+    if (token === 'mUrtAzA11mUnIrA22') {
+        const posts = await Post.fetch()
+        res.status(200).json(posts);
+    }
+    else {
+        res.status(401).json({ "error": "Unauthorized" });
+    }
+})
+
+app.get('/post/:title', async (req, res) => {
+    var token = req.get('token')
+    if (token === 'mUrtAzA11mUnIrA22') {
+        const { title } = req.params;
+        const post = await Post.get(title)
+        res.status(200).json(post);
+    }
+    else {
+        res.status(401).json({ "error": "Unauthorized" });
+    }
+})
+
+
+
+// export 'app'
+module.exports = app
