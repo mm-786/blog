8b8de24e4977ab0bccc7abb669cee83b7e3957b6
M Mundrawala <mundrawala18@gmail.com>
2021-12-25 21:27:16 +0530

    GS,JNC,K

---
+++ index.js
@@ -0,0 +1,26 @@
+new Vue({
+    el: '#mrtz',
+    data: {
+     posts:[],
+    },
+    mounted() {
+      const myHeaders = new Headers();
+      myHeaders.append("Content-Type", "application/json");
+      myHeaders.append("token", "mUrtAzA11mUnIrA22");
+
+      const requestOptions = {
+        method: 'GET',
+        headers: myHeaders,
+        redirect: 'follow'
+      };
+
+      fetch("https://api.mundrawala.ml/posts", requestOptions)
+        .then(response => response.text())
+        .then(result => {
+          const data = JSON.parse(result);
+          this.posts= data.items;
+          
+        })
+        .catch(error => console.log('error', error));
+    },
+  })
---
+++ riter.js
@@ -0,0 +1,66 @@
+new Vue({
+  el: '#mrtz',
+  data: {
+    message: "",
+    title: '',
+    tags: '',
+    auth: false,
+    load:false,
+  },
+  mounted() {
+    if (window.localStorage.getItem('mrtz') == 'munira') {
+      this.auth = true;
+    }
+    $('#summernote').summernote({
+      tabsize: 4,
+      height: 500,
+      toolbar: [
+        ['style', ['style', 'undo', 'redo']],
+        ['font', ['fontname', 'fontsize', 'bold', 'underline', 'clear', 'italic', 'strikethrough', 'superscript', 'subscript']],
+        ['color', ['color']],
+        ['para', ['ul', 'ol', 'paragraph']],
+        ['height', ['height']],
+        ['insert', ['link', 'picture', 'video', 'table']],
+        ['view', ['fullscreen', 'codeview', 'help']],
+
+      ]
+    });
+  },
+  methods: {
+    getCode() {
+      const cont = $('#summernote')
+      this.message = encodeURIComponent(cont.summernote('code'));
+
+    },
+    submitPost() {
+      this.load = true;
+      this.getCode();
+      const myHeaders = new Headers();
+      myHeaders.append("Content-Type", "application/json");
+      myHeaders.append("token", "mUrtAzA11mUnIrA22");
+
+      const raw = JSON.stringify({
+        "title": this.title,
+        "content": this.message,
+      });
+
+      const requestOptions = {
+        method: 'POST',
+        headers: myHeaders,
+        body: raw,
+        redirect: 'follow'
+      };
+
+      fetch("https://api.mundrawala.ml/post", requestOptions)
+        .then(response => response.text())
+        .then(result => {
+          this.load = false
+          location.reload();
+          console.log(result)})
+        .catch(error => {
+          this.load=false
+          console.log('error', error)});
+
+    }
+  }
+})
