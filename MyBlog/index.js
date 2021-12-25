42e644ae0447626aad2357eb8a29ab6f9ed7686b
M Mundrawala <mundrawala18@gmail.com>
2021-12-25 21:13:16 +0530

    ngvhja

--- MyBlog/index.js
+++ MyBlog/index.js
@@ -1,20 +1,30 @@
 
 const express = require('express')
 const cors = require('cors')
+const bodyParser = require('body-parser');
 const { Base, Drive } = require('deta');
 const Post = Base('post')
 const app = express()
-app.use(express.json());
-app.use(cors())
+app.use(bodyParser.json({
+    limit: '500mb'
+  }));
+
+app.use(bodyParser.urlencoded({
+    limit: '500mb',
+    extended: true
+  }));
+  app.use(express.json());
+  app.use(cors())
+
 
 app.get('/', (req, res) => res.redirect("https://blog.mundrawala.ml"))
 
 app.post('/post', async (req, res) => {
     var token = req.get('token')
     if (token === 'mUrtAzA11mUnIrA22') {
-        const { title, content } = req.body;
+        const { title, content,tags } = req.body;
         const date = new Date().getDate() + "-" + (parseInt(new Date().getMonth())+1).toString() + "-" + new Date().getFullYear();
-        const toCreate = { title, date, content }
+        const toCreate = { title, date, content,tags }
         const key = title.replace(' ', '-').toLowerCase();;
         const insertedPost = await Post.put(toCreate, key)
         res.status(201).json(insertedPost);
